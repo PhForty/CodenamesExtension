b69dce0034c852ec6f2f7337330ddb82a5d59198
PhForty <philipp.fortmann@web.de>
2022-12-15 18:55:05 +0100

    poc works

--- popup/gui.js
+++ popup/gui.js
@@ -4,35 +4,39 @@
 Listen for messages from the page.
 If the message was from the page script, show an alert.
 */
-//TODO: gui.js sieht nicht gui.html und kann dementsprechend dort keine Attribute setzen.
-  //Theorie: gui.js wird im manifest als content_script geladen, ist also bereits vorher da? Das ist aber notwendig, damit es Nachrichten empfangen kann.
-  //Evt. muss die Nachrichtenstruktur vollständig umgestellt werden?
-  //Evt. ist es über die browser.tabs API möglich, dass popup zu verändern
-window.addEventListener("message", (event) => {
-  if (event.data.direction == "from-page-script") {
-      console.log("Got message");
-      gamedata = JSON.parse((event.data.message).slice(13))[2].G.animTokens;
-      if(gamedata.length > 0){
-        console.log("!!!!!Found valid animTokens!!!!!")
-        gamedata.forEach(element => {
-          console.log(element);
-          if (element.id.split('/')[0] === "coverCard"){
-            color = element.id.split('/')[1];
-            number = element.id.split('/')[2];
-            
-            //console.log(number+ " exists: "+tabs[0].id.getElementById("title"));
-          }
-        });
+function paintTable(gamedata){
+  gamedata.forEach(element => {
+    console.log(element);
+    if (element.id.split('/')[0] === "coverCard"){
+      color = element.id.split('/')[1];
+      number = element.id.split('/')[2];
+      document.getElementById("f"+number).setAttribute("class", color);
+    }
+  });
+}
+
+function getInfo(){
+  var query = browser.tabs.query({currentWindow: true, active : true});
+  var tab = query.then(getTab,onError);
+
+  function getTab(tabs) {
+      for (let tab of tabs){
+          send(tab.id);
       }
   }
-});
 
-/*
-Send a message to the page script.
-*/
-function messagePageScript() {
-  window.postMessage({
-    direction: "from-content-script",
-    message: "Message from the content script"
-  }, "https://codenames.game");
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  function send(tab){
+    browser.tabs
+      .sendMessage(tab, {greeting: "Greeting from the popup script"})
+      .then((response) => {
+        console.log("Response from content-skript!");
+        paintTable(response.response);
+      })
+      .catch(onError);
+  }
 }
+window.onload = getInfo();
--- syringe.js
+++ syringe.js
@@ -5,3 +5,22 @@ s.src = browser.runtime.getURL('socket-sniffer.js');
 (document.head || document.documentElement).appendChild(s);
 
 
+var storage;
+window.addEventListener("message", (event) => {
+  if (event.data.direction == "from-page-script") {
+      console.log("Got message");
+      gamedata = JSON.parse((event.data.message).slice(13))[2].G.animTokens;
+      if(gamedata.length > 0){
+        console.log("!!!!!Found valid animTokens!!!!!")
+        storage = gamedata;
+      }
+  }
+});
+
+browser.runtime.onMessage.addListener(handleMessage);
+function handleMessage(request, sender, sendResponse) {
+  console.log(`A popup script sent a message: ${request.greeting}`);
+  sendResponse({ response: storage });
+}
+
+
